77df5ab17d47495ece95933a7c9dbb7b6692844c
João Simões <nogravity00@outlook.com>
2017-01-22 17:12:45 +0000

    Added interface to normalise IniContainers

---
+++ src/SimpleSoft.IniParser/IIniNormalizer.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleSoft.IniParser
+{
+    /// <summary>
+    /// Normalizes raw <see cref="IniContainer"/> instances to meet
+    /// the INI standard.
+    /// </summary>
+    public interface IIniNormalizer
+    {
+        /// <summary>
+        /// Normalizes the <see cref="IniContainer"/> instance.
+        /// </summary>
+        /// <param name="container">The container to normalize</param>
+        void Normalize(IniContainer container);
+
+        /// <summary>
+        /// Normalizes the <see cref="IniContainer"/> instance.
+        /// </summary>
+        /// <param name="container">The container to normalize</param>
+        /// <returns>True if instance normalized successfully, otherwise false</returns>
+        bool TryNormalize(IniContainer container);
+    }
+}
