7e4d098b5a3840a4013f155bda3327bbda847bb2
João Simões <nogravity00@outlook.com>
2017-01-23 08:44:55 +0000

    Added unit tests project

---
+++ test/SimpleSoft.IniParser.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,49 @@
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 João Simões
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+
+using System;
+using System.Reflection;
+
+#if NET45
+using System.Runtime.InteropServices;
+#endif
+
+[assembly: AssemblyTitle("SimpleSoft.IniParser.Tests")]
+[assembly: AssemblyDescription("Unit tests for SimpleSoft.IniParser.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SimpleSoft")]
+[assembly: AssemblyProduct("SimpleSoft.IniParser")]
+[assembly: AssemblyCopyright("Copyright © 2016 João Simões")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if NET45
+[assembly: ComVisible(false)]
+[assembly: Guid("7717c6d9-89df-4199-bd2b-46c0b5bcbaf0")]
+#endif
+
+[assembly: CLSCompliant(false)]
+
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
