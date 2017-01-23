37acce29f79e883424fbdb379070e6a804922c80
João Simões <nogravity00@outlook.com>
2017-01-23 08:58:07 +0000

    Added tests for IniNormalizationOptions

---
+++ test/SimpleSoft.IniParser.Tests/Normalization/IniNormalizationOptionsTests.cs
@@ -0,0 +1,50 @@
+﻿using SimpleSoft.IniParser.Impl;
+using Xunit;
+
+namespace SimpleSoft.IniParser.Tests.Normalization
+{
+    public class IniNormalizationOptionsTests
+    {
+        [Fact]
+        public void GivenANormalizationOptionsInstanceUsingTheDefaultConstrutorThenIncludeEmptyCommentsMustBeFalse()
+        {
+            var options = new IniNormalizationOptions();
+            Assert.False(options.IncludeEmptyComments);
+        }
+
+        [Fact]
+        public void GivenANormalizationOptionsInstanceUsingTheDefaultConstrutorThenIncludeEmptySectionsMustBeFalse()
+        {
+            var options = new IniNormalizationOptions();
+            Assert.False(options.IncludeEmptySections);
+        }
+
+        [Fact]
+        public void GivenANormalizationOptionsInstanceUsingTheDefaultConstrutorThenIncludeEmptyPropertiesMustBeFalse()
+        {
+            var options = new IniNormalizationOptions();
+            Assert.False(options.IncludeEmptyProperties);
+        }
+
+        [Fact]
+        public void GivenANormalizationOptionsInstanceUsingTheDefaultConstrutorThenIsCaseSensitiveMustBeFalse()
+        {
+            var options = new IniNormalizationOptions();
+            Assert.False(options.IsCaseSensitive);
+        }
+
+        [Fact]
+        public void GivenANormalizationOptionsInstanceUsingTheDefaultConstrutorThenReplaceOnDuplicatedPropertiesMustBeFalse()
+        {
+            var options = new IniNormalizationOptions();
+            Assert.False(options.ReplaceOnDuplicatedProperties);
+        }
+
+        [Fact]
+        public void GivenANormalizationOptionsInstanceUsingTheDefaultConstrutorThenMergeOnDuplicatedSectionsMustBeFalse()
+        {
+            var options = new IniNormalizationOptions();
+            Assert.False(options.MergeOnDuplicatedSections);
+        }
+    }
+}
