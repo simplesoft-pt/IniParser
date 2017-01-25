27a2a364d63805c007929c451ee1c6727f96ea96
João Simões <nogravity00@outlook.com>
2017-01-25 00:09:34 +0000

    Added methods to normalize sections

--- src/SimpleSoft.IniParser/NormalizerExtensions.cs
+++ src/SimpleSoft.IniParser/NormalizerExtensions.cs
@@ -39,5 +39,40 @@
             destination = null;
             return false;
         }
+
+        /// <summary>
+        /// Creates a normalized <see cref="IniSection"/> using the provided one.
+        /// </summary>
+        /// <param name="normalizer">The normalizer to use</param>
+        /// <param name="source">The section to normalize</param>
+        /// <returns>The new section with the normalization result</returns>
+        public static IniSection Normalize(this IIniNormalizer normalizer, IniSection source)
+        {
+            var destination = new IniSection(source.Name);
+            normalizer.NormalizeInto(source, destination);
+
+            return destination;
+        }
+
+        /// <summary>
+        /// Creates a normalized <see cref="IniSection"/> using the provided one.
+        /// </summary>
+        /// <param name="normalizer">The normalizer to use</param>
+        /// <param name="source">The section to normalize</param>
+        /// <param name="destination">The section with the normalization result</param>
+        /// <returns>True if instance normalized successfully, otherwise false</returns>
+        public static bool TryNormalize(
+            this IIniNormalizer normalizer, IniSection source, out IniSection destination)
+        {
+            var tmpDestination = new IniSection(source.Name);
+            if (normalizer.TryNormalizeInto(source, tmpDestination))
+            {
+                destination = tmpDestination;
+                return true;
+            }
+
+            destination = null;
+            return false;
+        }
     }
 }
