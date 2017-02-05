aa43f09f709a50d4c75f098f9d6ebfa42af489c2
João Simões <nogravity00@outlook.com>
2017-02-05 01:31:33 +0000

    Added tests for serialization options

---
+++ test/SimpleSoft.IniParser.Tests/Serialization/IniSerializationOptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using SimpleSoft.IniParser.Impl;
+using Xunit;
+
+namespace SimpleSoft.IniParser.Tests.Serialization
+{
+    public class IniSerializationOptionsTests
+    {
+        [Fact]
+        public void GivenASerializationOptionsInstanceUsingTheDefaultConstrutorThenNormalizeBeforeSerializationMustBeTrue()
+        {
+            var options = new IniSerializationOptions();
+            Assert.True(options.NormalizeBeforeSerialization);
+        }
+
+        [Fact]
+        public void GivenASerializationOptionsInstanceUsingTheDefaultConstrutorThenCommentIndicatorMustBeSemicolon()
+        {
+            var options = new IniSerializationOptions();
+            Assert.Equal(';', options.CommentIndicator);
+        }
+
+        [Fact]
+        public void GivenASerializationOptionsInstanceUsingTheDefaultConstrutorThenPropertyNameValueDelimiterMustBeEqualSign()
+        {
+            var options = new IniSerializationOptions();
+            Assert.Equal('=', options.PropertyNameValueDelimiter);
+        }
+
+        [Fact]
+        public void GivenASerializationOptionsInstanceUsingTheDefaultConstrutorThenEmptyLineBeforeSectionMustBeFalse()
+        {
+            var options = new IniSerializationOptions();
+            Assert.False(options.EmptyLineBeforeSection);
+        }
+    }
+}
