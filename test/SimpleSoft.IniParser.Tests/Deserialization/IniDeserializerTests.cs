8c12b3468122523522f993f8d82dbf2baa936199
João Simões <nogravity00@outlook.com>
2017-02-05 01:10:17 +0000

    Added tests for deserializer

---
+++ test/SimpleSoft.IniParser.Tests/Deserialization/IniDeserializerTests.cs
@@ -0,0 +1,48 @@
+﻿using SimpleSoft.IniParser.Impl;
+using Xunit;
+
+namespace SimpleSoft.IniParser.Tests.Deserialization
+{
+    public class IniDeserializerTests
+    {
+        [Fact]
+        public void GivenADeserializerWithDefaultConstructorThenDefaultsAreUsed()
+        {
+            var serializer = new IniDeserializer();
+
+            Assert.Same(IniDeserializationOptions.Default, serializer.Options);
+            Assert.Same(IniNormalizer.Default, serializer.Normalizer);
+        }
+
+        [Fact]
+        public void GivenADeserializerReceivingCustomOptionsThenDefaultNormalizerIsUsed()
+        {
+            var options = new IniDeserializationOptions();
+            var serializer = new IniDeserializer(options);
+
+            Assert.Same(options, serializer.Options);
+            Assert.Same(IniNormalizer.Default, serializer.Normalizer);
+        }
+
+        [Fact]
+        public void GivenADeserializerReceivingCustomNormalizerThenDefaultOptionsAreUsed()
+        {
+            var normalizer = new IniNormalizer();
+            var serializer = new IniDeserializer(normalizer);
+
+            Assert.Same(IniDeserializationOptions.Default, serializer.Options);
+            Assert.Same(normalizer, serializer.Normalizer);
+        }
+
+        [Fact]
+        public void GivenADeserializerReceivingCustomParametersThenReferencesAreTheSame()
+        {
+            var options = new IniDeserializationOptions();
+            var normalizer = new IniNormalizer();
+            var serializer = new IniDeserializer(options, normalizer);
+
+            Assert.Same(options, serializer.Options);
+            Assert.Same(normalizer, serializer.Normalizer);
+        }
+    }
+}
