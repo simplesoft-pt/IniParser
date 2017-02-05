c3da53d2414bc98d0239a75bb968111dd4eb6a0b
João Simões <nogravity00@outlook.com>
2017-02-05 02:03:23 +0000

    Added deserialization test for standard format, no errors

---
+++ test/SimpleSoft.IniParser.Tests/Deserialization/IniDeserializerFromStringTests.cs
@@ -0,0 +1,38 @@
+﻿using SimpleSoft.IniParser.Impl;
+using Xunit;
+
+namespace SimpleSoft.IniParser.Tests.Deserialization
+{
+    public class IniDeserializerFromStringTests
+    {
+        [Fact]
+        public void GivenADeserializerWithDefaultConstructorWhenDeserializingFromStringThenStandardContainerMatch()
+        {
+            var deserializer = new IniDeserializer();
+            var container = deserializer.DeserializeAsContainer(StandardNoErrors);
+
+            Assert.NotNull(container);
+        }
+
+        #region Test Data
+
+        private const string StandardNoErrors =
+@";gc 01
+;gc 02
+GP01=gp 01 value
+GP02=gp 02 value
+[S01]
+;s01 c01
+;s01 c02
+S01P01=s01 p01 value
+S01P02=s01 p02 value
+[S02]
+;s02 c01
+;s02 c02
+S02P01=s02 p01 value
+S02P02=s02 p02 value
+";
+
+        #endregion
+    }
+}
