054ff02a4a53fe66d7ff92298662525254ac4b30
João Simões <nogravity00@outlook.com>
2017-02-07 00:06:50 +0000

    Added tests for deserialization options

---
+++ test/SimpleSoft.IniParser.Tests/Deserialization/IniDeserializationOptionsTests.cs
@@ -0,0 +1,36 @@
+﻿using SimpleSoft.IniParser.Impl;
+using Xunit;
+
+namespace SimpleSoft.IniParser.Tests.Deserialization
+{
+    public class IniDeserializationOptionsTests
+    {
+        [Fact]
+        public void GivenADeserializationOptionsInstanceUsingTheDefaultConstrutorThenNormalizeAfterDeserializationMustBeTrue()
+        {
+            var options = new IniDeserializationOptions();
+            Assert.True(options.NormalizeAfterDeserialization);
+        }
+
+        [Fact]
+        public void GivenADeserializationOptionsInstanceUsingTheDefaultConstrutorThenCommentIndicatorMustBeSemicolon()
+        {
+            var options = new IniDeserializationOptions();
+            Assert.Equal(';', options.CommentIndicator);
+        }
+
+        [Fact]
+        public void GivenADeserializationOptionsInstanceUsingTheDefaultConstrutorThenPropertyNameValueDelimiterMustBeEqualSign()
+        {
+            var options = new IniDeserializationOptions();
+            Assert.Equal('=', options.PropertyNameValueDelimiter);
+        }
+
+        [Fact]
+        public void GivenADeserializationOptionsInstanceUsingTheDefaultConstrutorThenFailOnInvalidLinesMustBeTrue()
+        {
+            var options = new IniDeserializationOptions();
+            Assert.True(options.FailOnInvalidLines);
+        }
+    }
+}
